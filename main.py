9153869ada48f6ed353b305179abfe62966ccaf5
Wizock <Rohaanerodasahmed@gmail.com>
2021-10-12 18:55:53 +1000

    wsgi

--- main.py
+++
@@ -1,5 +0,0 @@
-from src.views.login import *
-from src import app
-if __name__=='__main__':
-        
-    app.run(debug=True, port=8080, ssl_context=('certificates/cert.pem', 'certificates/key.pem'))
